b353f3b72a0bcc1c5b3af1f704b54fe44c52c284
Samyak <samyak15.sj@gmail.com>
2024-10-15 00:19:38 +0530

    initial commit

--- src/pages/api/generate-banner.js
+++ src/pages/api/generate-banner.js
@@ -4,7 +4,7 @@ import enhancePrompt from '../../utils/enhancePrompt';
 
 const together = new Together({ apiKey: process.env.TOGETHER_API_KEY });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
@@ -16,17 +16,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const response = await together.images.create({
       model: "black-forest-labs/FLUX.1-schnell-Free",
       prompt: enhancedPrompt,
-      width: 512,
-      height: 512,
+      width: 1440,
+      height: 576,
       n: 1,
     });
 
     // Assuming the response structure contains the image URL
     const imageUrl = response.data[0].url;
+    console.log('Generated Twitter banner:', imageUrl);
 
     res.status(200).json({ imageUrl });
   } catch (error) {
-    console.error('Error generating logo:', error);
-    res.status(500).json({ message: 'Error generating logo' });
+    console.error('Error generating Twitter banner:', error);
+    res.status(500).json({ message: 'Error generating Twitter banner' });
   }
 }
--- src/pages/api/generate-logo.js
+++ src/pages/api/generate-logo.js
@@ -4,7 +4,7 @@ import enhancePrompt from '../../utils/enhancePrompt';
 
 const together = new Together({ apiKey: process.env.TOGETHER_API_KEY });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req  ,res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
@@ -16,8 +16,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const response = await together.images.create({
       model: "black-forest-labs/FLUX.1-schnell-Free",
       prompt: enhancedPrompt,
-      width: 1500,
-      height: 500,
+      width: 512,
+      height: 512,
       n: 1,
     });
 
@@ -26,7 +26,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     res.status(200).json({ imageUrl });
   } catch (error) {
-    console.error('Error generating Twitter banner:', error);
-    res.status(500).json({ message: 'Error generating Twitter banner' });
+    console.error('Error generating logo:', error);
+    res.status(500).json({ message: 'Error generating logo' });
   }
 }
